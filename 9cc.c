2076eea72bcc5dcd39a9a0645ad0fd24f8f35799
Yoshiki Kurihara <clouder@gmail.com>
2019-10-31 17:14:34 +0900

    Add unary plus and minus

--- 9cc.c
+++ 9cc.c
@@ -155,10 +155,12 @@ Node *new_node_num(int val) {
 }
 
 // expr    = mul ("+" mul | "-" mul)*
-// mul     = primary ("*" primary | "/" primary)*
+// mul     = unary ("*" unary | "/" unary)*
+// unary   = ("+" | "-")? primary
 // primary = num | "(" expr ")"
 
 Node *primary();
+Node *unary();
 Node *mul();
 Node *expr();
 
@@ -172,14 +174,22 @@ Node *primary() {
   }
 }
 
+Node *unary() {
+  if (consume('+'))
+    return primary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), unary());
+  return primary();
+}
+
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
